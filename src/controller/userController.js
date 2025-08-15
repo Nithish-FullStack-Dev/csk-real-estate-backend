589f5b4d85f3ee8e43330f4c08cd5954f5767637
Nithish <nithishsunny1234@gmail.com>
2025-08-15 10:55:15 +0530

    user controller path changed

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -61,6 +61,17 @@ export const getAllUsers = async (req, res) => {
   }
 };
 
+export const getLoggedInUser = async (req, res) => {
+  try {
+    const user = req.user;
+    if (!user) res.status(404).json({ message: "logged in user not found" });
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 export const getUserWithRole = async (req, res) => {
   try {
     const { userId } = req.params;
