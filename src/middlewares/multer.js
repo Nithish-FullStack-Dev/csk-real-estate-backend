353cc896ad72ff8b6c64091c58d9d6b2d289d315
Srirammohan03 <sriram9491@gmail.com>
2026-03-03 16:55:03 +0530

    added auditlog

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,35 +1,51 @@
-// src/middlewares/multer.js
 import multer from "multer";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// recreate __dirname (ESM)
+// recreate __dirname (ESM safe)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const uploadDir = path.join(__dirname, "..", "uploads", "tmp");
+// Base uploads folder
+const baseUploadDir = path.join(__dirname, "..", "uploads");
 
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
+// Ensure base folder exists
+if (!fs.existsSync(baseUploadDir)) {
+  fs.mkdirSync(baseUploadDir, { recursive: true });
 }
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDir);
+    let folder = "others";
+
+    if (file.mimetype.startsWith("image/")) {
+      folder = "images";
+    } else if (file.mimetype === "application/pdf") {
+      folder = "pdfs";
+    }
+
+    const finalPath = path.join(baseUploadDir, folder);
+
+    if (!fs.existsSync(finalPath)) {
+      fs.mkdirSync(finalPath, { recursive: true });
+    }
+
+    cb(null, finalPath);
   },
+
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(
-      null,
-      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname),
-    );
+
+    const ext = path.extname(file.originalname);
+
+    cb(null, uniqueSuffix + ext);
   },
 });
 
 export const upload = multer({
   storage,
   limits: {
-    fileSize: 100 * 1024 * 1024, // 🔥 100MB
+    fileSize: 20 * 1024 * 1024, 
   },
 });
