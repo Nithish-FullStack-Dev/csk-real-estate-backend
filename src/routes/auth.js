11a9f8a81f7dae4780ab72a52372d3be6b5117a5
Nithish <nithishsunny1234@gmail.com>
2026-03-11 18:34:18 +0530

    started with sucure login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { sendOtp, verifyOtp } from "../controller/authController.js";
+import {
+  secureVerifyOtp,
+  sendOtp,
+  verifyOtp,
+} from "../controller/authController.js";
 
 const router = express.Router();
 
 router.post("/send-otp", sendOtp);
 router.post("/verify-otp", verifyOtp);
+router.post("/verify-otp-access", secureVerifyOtp);
 
 export default router;
